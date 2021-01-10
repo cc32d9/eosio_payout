725e7011f5fbb0689104a5111f9818eb8c27af47
cc32d9 <cc32d9@gmail.com>
2021-01-10 01:43:48 +0000

    Work in progress: PONY payer example

---
+++ examples/payout_client_example/README.md
@@ -0,0 +1,79 @@
+# Payout Engine client example
+
+## Test Token on Telos Testnet
+
+```
+Token contract: ponytokenxxx
+Symbol: 4,PONY
+Max supply: 10000000.0000 PONY
+
+Mint account: ponymintmint
+Treasury account: ponytreasure
+Payer account: ponypayerxxx
+
+
+alias tTcleos='cleos -v -u https://testnet.persiantelos.com'
+
+# Assuming EOSIO 2.x and CDT 1.7.0 packages are installed, see
+# https://github.com/EOSIO/eos/releases
+# https://github.com/EOSIO/eosio.cdt/releases
+
+# Assuming the private keys are imported in a wallet and it's unlocked.
+
+# compile the token contract
+mkdir /opt/src
+cd /opt/src
+git clone https://github.com/EOSIO/eosio.contracts.git
+cd eosio.contracts/contracts/eosio.token/src
+eosio-cpp -I ../include/ eosio.token.cpp
+
+# upload the token contract to the blockchain account
+tTcleos system buyram ponytokenxxx ponytokenxxx -k 100
+tTcleos set contract ponytokenxxx . eosio.token.wasm eosio.token.abi 
+
+# we delegate the "create" action to ponymintmint, so that it can create new currencies
+tTcleos set account permission ponytokenxxx create '{"threshold": 1, "accounts":[{"permission":{"actor":"ponymintmint","permission":"active"},"weight":1}], "waits":[] }' active -p ponytokenxxx@active
+tTcleos set action permission ponytokenxxx ponytokenxxx create create -p ponytokenxxx@active
+
+# make the token contract immutable
+tTcleos set account permission ponytokenxxx active '{"threshold": 1, "accounts":[{"permission":{"actor":"eosio","permission":"active"},"weight":1}], "waits":[] }' owner -p ponytokenxxx@owner
+tTcleos set account permission ponytokenxxx owner '{"threshold": 1, "accounts":[{"permission":{"actor":"eosio","permission":"active"},"weight":1}], "waits":[] }' -p ponytokenxxx@owner
+
+
+# Create the token
+tTcleos push action ponytokenxxx create '["ponymintmint", "10000000.0000 PONY"]' -p ponytokenxxx@create
+
+# Issue 1M tokens and send them to treasury
+tTcleos push action ponytokenxxx issue '["ponymintmint", "1000000.0000 PONY", ""]' -p ponymintmint@active
+tTcleos push action ponytokenxxx transfer '["ponymintmint", "ponytreasure", "1000000.0000 PONY", "treasury issuance"]' -p ponymintmint
+
+# allocate 100k tokens to the payer account
+tTcleos push action ponytokenxxx transfer '["ponytreasure", "ponypayerxxx", "100000.0000 PONY", "preparing for payout"]' -p ponytreasure
+```
+
+## Setting up the schedule
+
+```
+# buy enough RAM for the payments
+tTcleos system buyram ponypayerxxx ponypayerxxx -k 1000
+
+# register the schedule
+tTcleos push action payoutengine newschedule '["ponypayerxxx", "pony", "ponytokenxxx", "1.0000 PONY", "A pony!"]' -p ponypayerxxx
+```
+
+## Payer server environment
+
+```
+apt update && apt install -y mariadb-server git curl
+
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
+
+
+cd /opt
+git clone https://github.com/cc32d9/eosio_payout.git
+cd eosio_payout/examples/payout_client_example
+
+mysql < ponypayer_dbsetup.sql
+
+npm install
---
+++ examples/payout_client_example/ponypayer_daemon.js
@@ -0,0 +1,320 @@
+#!/usr/bin/env node
+
+/*
+  Copyright 2020 cc32d9@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+const config    = require('config');
+const fetch     = require('node-fetch');
+const mariadb   = require('mariadb');
+
+const { Api, JsonRpc, RpcError } = require('eosjs');
+const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');
+const { TextEncoder, TextDecoder } = require('util');
+
+
+
+const url        = config.get('url');
+const engineacc  = config.get('engineacc');
+const payeracc   = config.get('adminacc');
+const payerkey   = config.get('adminkey');
+
+const book_batch_size   = config.get('book_batch_size');
+
+const schedule_name = config.get('schedule.name');
+const currency = config.get('schedule.currency');
+const currency_precision = config.get('schedule.currency_precision');
+const currency_multiplier = 10**currency_precision;
+
+const pool = mariadb.createPool(config.get('mariadb_server'));
+
+
+const timer_keepalive = config.get('timer.keepalive');  // this one prints the keepalive message to console
+const timer_check_accounts = config.get('timer.check_accounts'); // this one checks if new EOSIO accounts exist on chain
+const timer_fullscan = config.get('timer.fullscan'); // this one scans all recipients
+const timer_partialscan = config.get('timer.partialscan'); // this one scans only recipients with new payments
+
+
+const sigProvider = new JsSignatureProvider([payerkey]);
+const rpc = new JsonRpc(url, { fetch });
+const api = new Api({rpc: rpc, signatureProvider: sigProvider,
+                     textDecoder: new TextDecoder(), textEncoder: new TextEncoder()});
+
+console.log("Started payer daemon using " + url);
+console.log("Schedule name: " + schedule_name);
+
+var full_scan_running = false;
+var latest_payment_timestamp = 0;
+var book_batch = new Array();
+
+setInterval(keepaalive, timer_keepalive);
+setTimeout(check_accounts, timer_check_accounts);
+setTimeout(partialscan, timer_partialscan);
+
+fullscan();
+
+
+async function keepaalive() {
+    console.log('keepalive');
+}
+
+
+async function fullscan() {
+    full_scan_running = true;
+    console.log("fullscan started");
+
+    let booked_totals = new Map();
+    let counter = 0;
+    let conn;
+
+    try {
+        await fetch_booked_totals(booked_totals);
+
+        conn = await pool.getConnection();
+        conn.queryStream
+        ("SELECT PAYMENTS.recipient AS recipient, SUM(amount) AS due, UNIX_TIMESTAMP(MAX(tstamp)) AS ts, memo " +
+         "FROM PAYMENTS " +
+         "INNER JOIN VALID_ACCOUNTS ON PAYMENTS.recipient=VALID_ACCOUNTS.account " +
+         "LEFT JOIN PAYMENT_MEMO ON PAYMENTS.recipient=PAYMENT_MEMO.recipient " +
+         "GROUP BY recipient")
+            .on("error", err => {
+                console.error(err);
+            })
+            .on("data", row => {
+                if( ! booked_totals.has(row.recipient) ||
+                    row.due > booked_totals.get(row.recipient) ) {
+
+                    counter++;
+                    
+                    if( row.ts > latest_payment_timestamp ) {
+                        latest_payment_timestamp = row.ts;
+                    }
+
+                    if( row.memo === null ) {
+                        row.memo = '';
+                    }
+
+                    push_book_batch(row.recipient, row.due, row.memo);
+                }
+            })
+            .on("end", () => {
+                if( book_batch.size > 0 ) {
+                    send_book_tx(book_batch.slice());
+                    book_batch = new Array();
+                }
+                console.log("fullscan: sent " + counter + " bookings");
+            });
+    } catch (err) {
+        console.error(err);
+    } finally {
+	if (conn) conn.release(); //release to pool
+    }
+
+    full_scan_running = false;
+    setTimeout(fullscan, timer_fullscan);
+}
+
+
+
+async function partialscan() {
+    if( !full_scan_running && latest_payment_timestamp > 0 ) {
+
+        let counter = 0;
+        let conn;
+
+        try {
+            conn = await pool.getConnection();
+            conn.queryStream
+            ("SELECT PAYMENTS.recipient AS recipient, SUM(amount) AS due, UNIX_TIMESTAMP(MAX(tstamp)) AS ts, memo " +
+             "FROM PAYMENTS " +
+             "INNER JOIN (SELECT DISTINCT recipient AS recipient FROM PAYMENTS " +
+             "    WHERE tstamp > FROM_UNIXTIME(" + latest_payment_timestamp + ")) X ON PAYMENTS.recipient=X.recipient " +
+             "INNER JOIN VALID_ACCOUNTS ON PAYMENTS.recipient=VALID_ACCOUNTS.account " +
+             "LEFT JOIN PAYMENT_MEMO ON PAYMENTS.recipient=PAYMENT_MEMO.recipient " +
+             "GROUP BY recipient")
+                .on("error", err => {
+                    console.error(err);
+                })
+                .on("data", row => {
+                    counter++;
+                    if( row.ts > latest_payment_timestamp ) {
+                        latest_payment_timestamp = row.ts;
+                    }
+
+                    if( row.memo === null ) {
+                        row.memo = '';
+                    }
+
+                    check_and_book({recipient: row.recipient,
+                                    due: parseFloat(row.due),
+                                    memo: row.memo});
+                })
+                .on("end", () => {
+                    if( book_batch.size > 0 ) {
+                        send_book_tx(book_batch.slice());
+                        book_batch = new Array();
+                    }
+                    console.log("partialscan: found  " + counter + " new payments");
+                });
+        } catch (err) {
+            console.error(err);
+        } finally {
+	    if (conn) conn.release(); //release to pool
+        }
+    }
+
+    setTimeout(partialscan, timer_partialscan);
+}
+
+
+async function check_accounts() {
+    let counter = 0;
+    let conn;
+    
+    try {
+        conn = await pool.getConnection();
+        conn.queryStream
+            ("SELECT DISTINCT recipient AS recipient " +
+             "FROM PAYMENTS " +
+             "LEFT JOIN VALID_ACCOUNTS ON PAYMENTS.recipient=VALID_ACCOUNTS.account " +
+             "WHERE VALID_ACCOUNTS.account IS NULL")
+                .on("error", err => {
+                    console.error(err);
+                })
+            .on("data", row => {
+                counter++;
+                check_if_valid_account(row.recipient);
+            })
+            .on("end", () => {
+                console.log("check_accounts: found  " + counter + " new valid accounts");
+            });
+    } catch (err) {
+        console.error(err);
+    } finally {
+	if (conn) conn.release(); //release to pool
+    }
+
+    setTimeout(check_accounts, timer_check_accounts);
+}
+    
+
+
+
+function push_book_batch(recipient, due, memo) {
+    book_batch.push({recipient: recipient,
+                     new_total: parseFloat(due).toFixed(currency_precision) + " " + currency,
+                     memo: memo});
+    if( book_batch.size >= book_batch_size ) {
+        send_book_tx(book_batch.slice());
+        book_batch = new Array();
+    }
+}
+
+
+
+async function fetch_booked_totals(totals_map, nextkey="") {
+    let response = await fetch(url + '/v1/chain/get_table_rows', {
+        method: 'post',
+        body:    JSON.stringify({
+            json: 'true',
+            code: engineacc,
+            scope: schedule_name,
+            table: 'recipients',
+            key_type: 'name',
+            lower_bound: nextkey
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    let data = await response.json();
+    data.rows.forEach(function(row) {
+        let amount = parseInt(row.booked_total)/currency_multiplier;
+        totals_map.set(row.account, amount.toFixed(currency_precision));
+    });
+
+    if( data.more ) {
+        return fetch_booked_totals(totals_map, data.next_key);
+    }
+
+    console.log("fetch_booked_totals: retrieved " + totals_map.size + " balances");
+    return;
+}
+
+
+async function check_and_book(row) {
+    let response = await fetch(url + '/v1/chain/get_table_rows', {
+        method: 'post',
+        body:    JSON.stringify({
+            json: 'true',
+            code: engineacc,
+            scope: schedule_name,
+            table: 'recipients',
+            key_type: 'name',
+            lower_bound: row.recipient,
+            limit: 1
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    let data = await response.json();
+    let amount = 0;
+    if( data.rows.size > 0 ) {
+        amount = parseInt(data.rows[0].booked_total)/currency_multiplier;
+    }
+
+    if( row.due > amount ) {
+        push_book_batch(row.recipient, row.due, row.memo);
+    }
+}
+
+
+async function send_book_tx(rows) {
+    try {
+        const result = await api.transact(
+            {
+                actions:
+                [
+                    {
+                        account: engineacc,
+                        name: 'bookm',
+                        authorization: [{
+                            actor: payeracc,
+                            permission: 'active'} ],
+                        data: {
+                            schedule_name: schedule_name,
+                            records: rows
+                        },
+                    }
+                ]
+            },
+            {
+                blocksBehind: 10,
+                expireSeconds: 60
+            }
+        );
+        console.info(bookm transaction ID: ', result.transaction_id);
+    } catch (e) {
+        console.error('ERROR: ' + e);
+    }
+}
+
+
+/*
+ Local Variables:
+ mode: javascript
+ indent-tabs-mode: nil
+ End:
+*/
